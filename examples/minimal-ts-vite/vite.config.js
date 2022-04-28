996a94732a96bf734d76973238150af8547c8c60
Dave Kozma <dkozma@adobe.com>
2022-04-28 11:03:33 -0400

    Updated input

--- examples/minimal-ts-vite/vite.config.js
+++ examples/minimal-ts-vite/vite.config.js
@@ -1,12 +1,19 @@
 const { resolve } = require('path');
 const { defineConfig } = require('vite');
+const fg = require('fast-glob');
+
+const input = fg.sync('examples/**/index.html').reduce((acc, path) => {
+  const key = path.split('/')[1];
+  return {
+    ...acc,
+    [key]: resolve(__dirname, path),
+  };
+}, {});
 
 module.exports = defineConfig({
   build: {
     rollupOptions: {
-      input: {
-        quickStart: resolve(__dirname, 'examples/quick-start/index.html'),
-      },
+      input,
     },
   },
 });
